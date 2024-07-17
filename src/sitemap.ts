c4a009d7b460085e7261d95d16c5cbf4c64052a3
DASTAN DOKTURBEKOV <dastanhayama@gmail.com>
2024-07-17 15:45:11 +0600

    seo updated

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -3,12 +3,12 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://localhost:3000',
+      url: 'https://www.intourist.pro',
       lastModified: new Date(),
       alternates: {
         languages: {
-          en: 'https://localhost:3000/en',
-          ru: 'https://localhost:3000/ru',
+          en: 'https://www.intourist.pro/en',
+          ru: 'https://www.intourist.pro/ru',
         },
       },
     },
